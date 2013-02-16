5b8a5ea88e9ea84a01682a12ae4d0a078ee68c85
nakaji-dayo <nakaji.dayo@gmail.com>
2013-02-16 20:28:22 +0900

    connectionをシングルトン

--- routes/pblList.js
+++ routes/pblList.js
@@ -1,8 +1,15 @@
 var request = require('superagent');
+var Pbl = require('../models/pbl.js');
+
 exports.list = function(req, res){
     request.get('https://api.github.com/repos/beerbu/github-agile-tool/issues?labels=PBL')
             .end(function(httpRes) {
                 var issues = JSON.parse(httpRes.text);
+var i=0;
+issues.forEach(function(issue) {
+    issue.priority = i++;
+});
+                console.log(issues);
                 res.render('list', {title:"List", issues:issues});
             });
 };
