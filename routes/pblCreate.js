1ca19f580175b6fc560e5a67ca03f771d0972194
John Smith <wakai.hirofumi@gmail.com>
2013-02-17 09:38:45 +0900

    add label

--- routes/pblCreate.js
+++ routes/pblCreate.js
@@ -8,9 +8,9 @@ exports.index = function(req, res) {
 exports.save = function(req, res) {
     console.log(req.body);
 	var token= require('passport').session.accessToken;
-	var post_data = {"title":req.body.title, "body":req.body.body};
+	var post_data = {"title":req.body.title, "body":req.body.body, "labels":["PBL"]};
     var postStr = JSON.stringify(post_data);
-    var headers = {'Content-Type': 'application/json', 'Content-Length': postStr.length, 'Authorization': 'token ' + token};
+    var headers = {'Content-Type': 'application/json', 'Content-Length': postStr.length}//, 'Authorization': 'token ' + token};
     var options = {host: 'api.github.com', port: 443, path: '/repos/' + req.session.passport.user.username + '/' + req.body.project + '/issues', method: 'POST', headers: headers};
 	console.log(options);
 	var http = require('https');
@@ -20,7 +20,7 @@ exports.save = function(req, res) {
         response.on('data', function(data) {
             responseString += data;
 			console.log(responseString);
-            res.redirect('/pblCreate/index/' + (req.body.title));
+            res.redirect('/pblCreate/index/' + (req.body.project));
         });
     });
     request.write(postStr);
